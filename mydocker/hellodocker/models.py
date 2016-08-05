e237337bb4314f951e93b1bf46fd608694505d18
spacetime <jingweiming-sp@outlook.com>
2016-08-05 11:30:36 +0800

    fix

--- mydocker/hellodocker/models.py
+++ mydocker/hellodocker/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class RtCatApp(models.Model):
     create_date = models.DateTimeField('date create',auto_now=True)
+    number = models.IntegerField('app number',default=0)
     def __str__(self):
         return self.create_date.__str__()
--- mydocker/hellodocker/views.py
+++ mydocker/hellodocker/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render
 from .models import RtCatApp
 from django.urls import reverse
 import logging
-
+from .docker_interface import Docker
 
 logger = logging.getLogger(__name__)
+docker = Docker()
 
 def index(request):
     app_list = RtCatApp.objects.all()
@@ -14,15 +15,14 @@ def index(request):
     return render(request,'hellodocker/index.html',context)
 
 def api(request,app_id):
+    count = RtCatApp.objects.count()
     if request.method == "POST":
-        newApp = RtCatApp()
+        newApp = RtCatApp(number=count-1)
         newApp.save()
+        docker.create()
+        docker.start(num=count - 1)
     elif request.method == "DELETE":
-        RtCatApp.objects.filter(id = app_id).delete()
+        app = RtCatApp.objects.filter(id=app_id)
+        docker.remove(num=app.first().number)
+        app.delete()
     return HttpResponse()
-
-def deleteApp(request):
-    return HttpResponseRedirect(reverse('hellodocker:index'))
-
-def addApp(request):
-    return HttpResponseRedirect(reverse('hellodocker:index'))
